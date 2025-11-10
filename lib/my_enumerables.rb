9d4de7ec209c0259266db1b88a449e6fe594e334
jbrenesp <josembrenes@gmail.com>
2025-11-10 16:18:11 +0200

    implemented my_each and my_each-with_index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -7,5 +7,18 @@ end
 # your enumerable module will have access
 # to this method
 class Array
-  # Define my_each here
+  def my_each
+    for i in 0...self.length
+      yield(self[i])
+    end
+    self
+  end
+  
+  def my_each_with_index
+    for i in 0...self.length
+      yield(self[i], i)
+    end
+    self
+  end
 end
+
